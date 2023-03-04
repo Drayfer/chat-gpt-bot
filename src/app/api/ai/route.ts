cbbe53331115b171c2dff47d3a037f70e9a19221
OlehMikhailychenko <devlydexter@gmail.com>
2023-03-04 03:37:53 +0300

    add prisma

---
+++ src/app/api/ai/route.ts
@@ -0,0 +1,80 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { Cors } from "@/lib/cors";
+import prisma from "@/lib/prismadb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+import { authOptions } from "../../../pages/api/auth/[...nextauth]";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+interface Question {
+  question: string;
+  chatSession: number;
+}
+
+interface UserSession {
+  user: {
+    email: string;
+  };
+}
+
+export async function POST(request: Request) {
+  const {
+    user: { email },
+  } = (await getServerSession(authOptions)) as UserSession;
+  const body: Question = await request.json();
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: body.question,
+      temperature: 0.5,
+      max_tokens: 800,
+    });
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    await prisma.chat.create({
+      data: {
+        message: body.question,
+        session: body.chatSession,
+        userId: user?.id,
+      },
+    });
+    return NextResponse.json(
+      { answer: response.data.choices[0].text },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(err, { status: 500 });
+  }
+}
+
+export async function GET(request: Request) {
+  const {
+    user: { email },
+  } = (await getServerSession(authOptions)) as UserSession;
+  const chat = await prisma.user.findUnique({
+    where: { email },
+    select: {
+      chat: {
+        orderBy: {
+          session: "desc",
+        },
+        take: 1,
+        select: {
+          session: true,
+        },
+      },
+    },
+  });
+  if (chat?.chat.length) {
+    const chatSession = chat?.chat[0]?.session;
+    return NextResponse.json({ chatSession: chatSession + 1 }, { status: 200 });
+  }
+  return NextResponse.json({ chatSession: 0 }, { status: 200 });
+}
