a178eef803c02dee1bbe79fb48b3fa8b76998f2b
OlehMikhailychenko <devlydexter@gmail.com>
2023-03-18 13:01:45 +0300

    add ads script

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,6 +7,11 @@ export default function Head() {
         name="description"
         content="Chat-bot GPT. Get an answer from the smartest neural network."
       />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1192136275433069"
+        crossOrigin="anonymous"
+      ></script>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
