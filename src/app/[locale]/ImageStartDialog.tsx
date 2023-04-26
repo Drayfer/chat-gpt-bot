ee1cf7d95bea06c1d736b684200adf58119b6306
OlehMikhailychenko <devlydexter@gmail.com>
2023-04-26 21:03:29 +0300

    add image generate examples

--- src/app/[locale]/ImageStartDialog.tsx
+++ src/app/[locale]/ImageStartDialog.tsx
@@ -1,17 +1,43 @@
 "use client";
 
+import { images } from "@/constants";
+import { CheckCircleOutlined } from "@ant-design/icons";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import { useMemo } from "react";
 
 export default function ImageStartDialog() {
   const t = useTranslations("app");
+
+  const randomNumber = useMemo(() => {
+    return Math.floor(Math.random() * images.length);
+  }, []);
+
   return (
     <>
       <div className="text-3xl flex justify-center items-center flex-col h-full">
         <p>Midjourney</p>
-        <p className="text-base text-center mt-4 font-light">
-          {t("askImage")}.
+        <p className="text-base text-center mt-4 font-light">{t("askImage")}</p>
+        <p className="text-base text-center font-semibold flex items-center mt-3">
+          <CheckCircleOutlined className="text-green-500 pr-2" /> {t("useEng")}.
         </p>
-        {/* <p className="text-base text-center font-light">{t("useEng")}...</p> */}
+        <div className="bg-slate-700 p-4 text-center mt-6 w-4/6">
+          <div className="mb-2 font-normal text-lg underline">
+            {t("example")}:
+          </div>
+          <div className="text-sm font-thin font-mono">
+            {images[randomNumber].text}
+          </div>
+          <div className="w-2/3 mt-3 md:w-1/5 mx-auto">
+            <Image
+              src={images[randomNumber].img}
+              alt="image_example"
+              width={0}
+              height={0}
+              style={{ width: "100%", height: "auto" }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/constants/index.ts
@@ -0,0 +1,74 @@
+export const images = [
+  {
+    text: "A humanoid figure standing in front of an oval shaped window, gazing upon a spaceship the size of a planet emerging behind the horizon. Clouds are breaking, the atmosphere appears clear under the light of an artificial star. 8k HD",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-Y5XEF65WWW7Qy5INzHDCcSw4/image.webp",
+  },
+  {
+    text: "T-Rex having a buffet at the Last Supper, detailed painting",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-G7f3z8cWNr1Zcmo7uaOUF9Mb/image.webp",
+  },
+  {
+    text: "A photo realistic image of a stairway leading to the glass facade of a concrete building submerged into the earth surrounded by wild grass with mountains in the background and a clear sky",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-SApyfDYgwhYmIm1XKvJw4nVZ/image.webp",
+  },
+  {
+    text: "a photo of a dog wearing pants",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-cjNO5jXXM9NnphIMbKaPpUJY/image.webp",
+  },
+  {
+    text: "A 1984 Pontiac Fiero catches fire in the middle of a highway, photorealistic",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-9mXtLOtrgDNCdrLoQ4ijdg5i/image.webp",
+  },
+  {
+    text: "A blue bird holding a red umbrella, storybook illustration",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-Gw3ZXcmnMoPk6JBKKfyMrGhl/image.webp",
+  },
+  {
+    text: "Digital Art, Two carrots waiting at a bus stop, Victorian era England",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-KI4hbxwZq7XRbCiKeaEGzidj/image.webp",
+  },
+  {
+    text: "a humanoid wearing a futuristic dragon themed Armor made of gold crystals and covered in arcane runes, standing in the ruins of an ancient civilization, HD digital art",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-CL9rGJgAEwSkEMoUIHEg9oZe/image.webp",
+  },
+  {
+    text: "Interior design, a living room from the 90s",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-HmoLOKUweqGUDIMAcyppTcT5/image.webp",
+  },
+  {
+    text: "A glass of water refracting a plush dinosaur",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-3i0NigOyQb8CDZeWtEs89r8f/image.webp",
+  },
+  {
+    text: "astronaut floating in space with tortoise shell cat in arms, digital art",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-7cIzrq4GR3ihQDyzcp4H2YyN/image.webp",
+  },
+  {
+    text: "Three capybaras in tuxedos holding martinis, digital art",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-ONUtvsATw3PRPRu1y1wXfFoG/image.webp",
+  },
+  {
+    text: "photo of house with the design of a strawberry",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-mEV023MBiS8SlgSTK5oLhSUg/image.webp",
+  },
+  {
+    text: "A Chihuahua in an alehouse enjoying an after work pint, photo real",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-KHqj3HXDMeoNhwei7BBFyibi/image.webp",
+  },
+  {
+    text: "A cathedral stands alongside the sea in the midst of a spectacular sunset, realistic",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-vfV2Bps5N53SLLdN99T8m3n9/image.webp",
+  },
+  {
+    text: "A photo taken inside The Backrooms",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-WG0860OYiNGSWWWLkalPo68A/image.webp",
+  },
+  {
+    text: "A crowded street in San Francisco after the aliens took over the planet",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-ep16wF0R1VHSso3STXBmAqA5/image.webp",
+  },
+  {
+    text: "Spongebob as a soldier during WW2 landing in the beach of Normandy",
+    img: "https://openai-labs-public-images-prod.azureedge.net/user-MlkIgOvgIajombPhUhj9mfe9/generations/generation-iMeFVdaAxsVYEr0qlbxr2DYc/image.webp",
+  },
+];
