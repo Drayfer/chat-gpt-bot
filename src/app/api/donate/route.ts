c5d80c8289b656fdce8a7cb2d8ca8541e3ffb1f6
OlehMikhailychenko <devlydexter@gmail.com>
2023-05-01 15:35:31 +0300

    add text donate route

---
+++ src/app/api/donate/route.ts
@@ -0,0 +1,23 @@
+import client from "@/lib/prismadb";
+import { getServerSession } from "next-auth/next";
+import { NextResponse } from "next/server";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { UserSession } from "../ai/route";
+interface IBody {
+  text: string;
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    console.log(111, body);
+    return NextResponse.json(
+      { data: "ok" },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    return NextResponse.json(err, { status: 500 });
+  }
+}
