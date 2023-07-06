4bd77f52d0e17d3d653ed27aa9bed729ceb31b29
OlehMikhailychenko <devlydexter@gmail.com>
2023-07-06 21:33:05 +0300

    fix image openai key

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
 
     const keyData = await client.settings.findFirst({
       where: {
-        openaiKey: {
+        openaiKeyPaid: {
           not: "",
         },
       },
