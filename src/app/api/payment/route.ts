296c3653dd84c26331ce2640f78414f033bf0ad9
OlehMikhailychenko <devlydexter@gmail.com>
2024-02-13 23:10:09 +0200

    update nodemailer, comment send emails

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -7,27 +7,27 @@ import { UserSession } from "../ai/route";
 
 export async function GET() {
   try {
-    const {
-      user: { email },
-    } = (await getServerSession(authOptions)) as UserSession;
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ukr.net",
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: process.env.NODEMAILER_EMAIL, // generated ethereal user
-        pass: process.env.NODEMAILER_EMAIL_PASSWORD, // generated ethereal password
-      },
-    });
-     const { data } = await axios(
-       "https://diaka.ua/api/v1/message/stats?action=recent&conveyorHash=ledVV43_827rc2Wroo7NLOs5tJrdfnja&params%5Blimit%5D=10&params%5Btest%5D=1"
-     );
-     await transporter.sendMail({
-       from: process.env.NODEMAILER_EMAIL, // sender address
-       to: "aigptchatbot@gmail.com", // list of receivers
-       subject: "Check Paid Access!!!", // Subject line
-       text: `Check paid access to ${email} \n ${JSON.stringify(data)}`,
-     });
+    // const {
+    //   user: { email },
+    // } = (await getServerSession(authOptions)) as UserSession;
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtp.ukr.net",
+    //   port: 465,
+    //   secure: true, // true for 465, false for other ports
+    //   auth: {
+    //     user: process.env.NODEMAILER_EMAIL, // generated ethereal user
+    //     pass: process.env.NODEMAILER_EMAIL_PASSWORD, // generated ethereal password
+    //   },
+    // });
+    //  const { data } = await axios(
+    //    "https://diaka.ua/api/v1/message/stats?action=recent&conveyorHash=ledVV43_827rc2Wroo7NLOs5tJrdfnja&params%5Blimit%5D=10&params%5Btest%5D=1"
+    //  );
+    //  await transporter.sendMail({
+    //    from: process.env.NODEMAILER_EMAIL, // sender address
+    //    to: "aigptchatbot@gmail.com", // list of receivers
+    //    subject: "Check Paid Access!!!", // Subject line
+    //    text: `Check paid access to ${email} \n ${JSON.stringify(data)}`,
+    //  });
 
     return NextResponse.json({ status: "ok" }, { status: 200 });
   } catch (err) {
--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -7,26 +7,26 @@ import { UserSession } from "../ai/route";
 
 export async function GET(request: Request) {
   try {
-    const {
-      user: { email },
-    } = (await getServerSession(authOptions)) as UserSession;
+    // const {
+    //   user: { email },
+    // } = (await getServerSession(authOptions)) as UserSession;
 
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ukr.net",
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: process.env.NODEMAILER_EMAIL, // generated ethereal user
-        pass: process.env.NODEMAILER_EMAIL_PASSWORD, // generated ethereal password
-      },
-    });
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtp.ukr.net",
+    //   port: 465,
+    //   secure: true, // true for 465, false for other ports
+    //   auth: {
+    //     user: process.env.NODEMAILER_EMAIL, // generated ethereal user
+    //     pass: process.env.NODEMAILER_EMAIL_PASSWORD, // generated ethereal password
+    //   },
+    // });
 
-    await transporter.sendMail({
-      from: process.env.NODEMAILER_EMAIL, // sender address
-      to: "aigptchatbot@gmail.com", // list of receivers
-      subject: "Open Access", // Subject line
-      text: email, // plain text body
-    });
+    // await transporter.sendMail({
+    //   from: process.env.NODEMAILER_EMAIL, // sender address
+    //   to: "aigptchatbot@gmail.com", // list of receivers
+    //   subject: "Open Access", // Subject line
+    //   text: email, // plain text body
+    // });
 
     return NextResponse.json({ data: "ok" }, { status: 200 });
   } catch (err) {
